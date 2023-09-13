fc9cdc151270840588eeaff2a276967f7ce1a3fe
Lauren Rogers <lfrog@unc.edu>
2023-09-13 12:06:10 -0400

    "Progress on Exercise 1"

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,7 +1,20 @@
 """EX01 - Chardle - A cute step toward Wordle."""
 
 __author__ = "730711512"
-str = input("My name is Lauren")
-print("Hello, " + input("My name is Lauren"))
+
+user_input1: str = "bakes"
+user_input2: str = "b"
+
+if(str(user_input1[0])) = str(lauren[0]):
+   print("Hooray!")
+
+if(str(user_input1[1])):
+   
+if(str(user_input1[2])):
+   
+if(str(user_input2[3])):
+   
+if(str(user_input1[3])):
+   
 
 
