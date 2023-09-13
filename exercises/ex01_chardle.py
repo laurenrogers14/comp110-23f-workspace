1826e16286355162c12def1392f028aca06ca7d6
Lauren Rogers <lfrog@unc.edu>
2023-09-13 15:14:13 -0400

    "Progress on Exercise 1"

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,18 +2,19 @@
 __author__ = "730711512"
  
 word_chardle: str = input("Enter a 5-character word: ")
-letter_chardle: str = input("Enter a single character: ")
 
 if len(word_chardle) !=5:
-    print("Error: Word must contain 5 characters.")
+    print(" Error: Word must contain 5 characters. ")
     exit()
+    
+letter_chardle: str = input("Enter a single character: ")
 
 if len(letter_chardle) !=1:
-    print ("Error: Character must be a single character.")
+    print (" Error: Character must be a single character. ")
     exit()
 
 count = 0
-print ("Searching for" + letter_chardle + "in" + word_chardle)
+print (" Searching for " + letter_chardle + " in " + word_chardle)
 
 if word_chardle[0] == letter_chardle:
     print(letter_chardle + " found at index 0 ")
@@ -31,4 +32,8 @@ if word_chardle[3] == letter_chardle:
     print(letter_chardle + " found at index 3 ")
     count += 1
 
-print (str(count) + "instance of" + letter_chardle + "found in" + word_chardle) 
+if word_chardle[4] == letter_chardle:
+    print(letter_chardle + " found at index 4 ")
+    count += 1
+
+print (str(count) + " instance of " + letter_chardle + " found in " + word_chardle) 
