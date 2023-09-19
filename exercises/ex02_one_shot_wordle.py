2b47d170b5686019578b9f2492899271f897089d
Lauren Rogers <lfrog@unc.edu>
2023-09-19 14:45:31 -0400

    "Progress on Exercise 1"

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -9,7 +9,7 @@ secret_word: len = 6
 while len(secret_word_guess) == secret_word:
     print("Not quite. Play again soon! ")
 if len(secret_word_guess) != secret_word:
-    print ("That was not 6 letters! Try again ")
+    print("That was not 6 letters! Try again ")
 if secret_word_guess == secret_word:
     print("Woo! You got it!")
 
@@ -20,53 +20,36 @@ YELLOW_BOX: str = "\U0001F7E8"
 
 while secret_word[0] == secret_word_guess:
     print("\U0001F7E9")
-    if secret_word [0] != secret_word_guess:
-        print ("\U00002B1C")
+    if secret_word[0] != secret_word_guess:
+        print("\U00002B1C")
         secret_word_guess_idx = secret_word_guess_idx + 1
 
 while secret_word[1] == secret_word_guess:
     print("\U0001F7E9")
-    if secret_word [1] != secret_word_guess:
-        print ("\U00002B1C")
+    if secret_word[1] != secret_word_guess:
+        print("\U00002B1C")
         secret_word_guess_idx = secret_word_guess_idx + 1
 
 while secret_word[2] == secret_word_guess:
     print("\U0001F7E9")
-    if secret_word [2] != secret_word_guess:
-        print ("\U00002B1C")
+    if secret_word[2] != secret_word_guess:
+        print("\U00002B1C")
         secret_word_guess_idx = secret_word_guess_idx + 1
 
 while secret_word[3] == secret_word_guess:
     print("\U0001F7E9")
-    if secret_word [3] != secret_word_guess:
-        print ("\U00002B1C")
+    if secret_word[3] != secret_word_guess:
+        print("\U00002B1C")
         secret_word_guess_idx = secret_word_guess_idx + 1
 
 while secret_word[4] == secret_word_guess:
     print("\U0001F7E9")
-    if secret_word [4] != secret_word_guess:
-        print ("\U00002B1C")
+    if secret_word[4] != secret_word_guess:
+        print("\U00002B1C")
         secret_word_guess_idx = secret_word_guess_idx + 1
 
 while secret_word[5] == secret_word_guess:
     print("\U0001F7E9")
-    if secret_word [5] != secret_word_guess:
-        print ("\U00002B1C")
+    if secret_word[5] != secret_word_guess:
+        print("\U00002B1C")
         secret_word_guess_idx = secret_word_guess_idx + 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
