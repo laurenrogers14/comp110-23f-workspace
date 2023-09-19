7950b6f19c322abe28e3b20999e6d53238d00550
Lauren Rogers <lfrog@unc.edu>
2023-09-19 13:39:29 -0400

    "Progress on Exercise 1"

---
+++ exercises/ex02_one_shot_wordle.py
@@ -0,0 +1,37 @@
+"""EX02 - One shot Wordle"""
+__author__ = "730711512"
+
+secret_word_guess: str = input("What is your 6-letter guess? ")
+secret_word: str = "python"
+
+secret_word: len = 6
+
+while len(secret_word_guess) == secret_word:
+    print("Not quite. Play again soon! ")
+if len(secret_word_guess) != secret_word:
+    print ("That was not 6 letters! Try again ")
+if secret_word_guess == secret_word:
+    print("Woo! You got it!")
+
+secret_word_guess_idx: chr = 0
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
+
+while secret_word[0] == secret_word_guess:
+    print("\U0001F7E9")
+if secret_word [0] != secret_word_guess:
+    print ("\U00002B1C")
+    secret_word_guess_idx = secret_word_guess_idx + 1
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lessons/guessing_game.py
@@ -0,0 +1,11 @@
+"""Guessing game that only completes when you guess the right number"""
+
+from random import randint
+
+secret: int = randint (1,10)
+
+guess: int = int(input ("Guess a number between 1 and 10: "))
+
+while guess != secret:
+    print("Wrong!")
+    
