85887ff0286b73adbf764b1b2ed99b1c4c3485fb
Lauren Rogers <lfrog@unc.edu>
2023-10-11 12:14:50 -0400

    "Progress on Exercise"

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -3,31 +3,37 @@
 __author__ = "730711512"
 
 
-i: int = 0
 def all(input1: list[int], input2: int) -> bool:
     """Given a list see if it completely matches the given integer. """
+    i: int = 0
     if len(input1) == 0:
         raise ValueError("max() arg is an empty List")
-    while len(input1) < input2:
+    while len(input1) > i:
         if input1[i] != input2:
             return False  
-    i = i + 1
+        i = i + 1
     return True
+
 def max(input3: list[int]) -> int: 
     """Given a list of integers, the function should return the largest one. """
+    i: int = 0
     if len(input3) == 0:
         raise ValueError("max() arg is an empty List")
+    max_number: int = input3[0]
     while len(input3) > i:
-        if input3[i] > input3[i]:
-            print(input3[i + 1])
-    i = i + 1
-    return True
+        if input3[i] > max_number:
+            max_number = input3[i]
+        i = i + 1
+    return max_number
 
 def is_equal(input5: list[int], input6: list[int]) -> bool:
     """Given two lists of integers, return True if every index in both lists are the same."""
-    while len(input5) <= len(input6):
-        if(input5[i]) == input6[i]:
-            return True
+    i: int = 0
+    if len(input5) == len(input6):
+        return False
+    while len(input5) > i:
+        if(input5[i]) != input6[i]:
+            return False
         i = i + 1
-    return False
+    return True
     
