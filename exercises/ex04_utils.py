11d67eab4c44345c38c5bfcfbf6710f232caa2b9
Lauren Rogers <lfrog@unc.edu>
2023-10-11 12:20:09 -0400

    "Progress on Exercise"

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,10 +1,11 @@
-"""EX04 - 'list' Utility Functions. """
+"""EX04 - 'list' Utility Functions."""
 
 __author__ = "730711512"
 
 
 def all(input1: list[int], input2: int) -> bool:
-    """Given a list see if it completely matches the given integer. """
+    """Given a list see if it completely matches the given integer."""
+    
     i: int = 0
     if len(input1) == 0:
         raise ValueError("max() arg is an empty List")
@@ -15,7 +16,8 @@ def all(input1: list[int], input2: int) -> bool:
     return True
 
 def max(input3: list[int]) -> int: 
-    """Given a list of integers, the function should return the largest one. """
+    """Given a list of integers, the function should return the largest one."""
+
     i: int = 0
     if len(input3) == 0:
         raise ValueError("max() arg is an empty List")
@@ -28,8 +30,9 @@ def max(input3: list[int]) -> int:
 
 def is_equal(input5: list[int], input6: list[int]) -> bool:
     """Given two lists of integers, return True if every index in both lists are the same."""
+
     i: int = 0
-    if len(input5) == len(input6):
+    if len(input5) == 0:
         return False
     while len(input5) > i:
         if(input5[i]) != input6[i]:
