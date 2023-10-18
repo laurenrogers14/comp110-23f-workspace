9cea9d1e0723c4b5d88e72148caeae986098ca15
Lauren Rogers <lfrog@unc.edu>
2023-10-18 12:28:27 -0400

    "progress on lessons"

--- lessons/sum.py
+++ lessons/sum.py
@@ -6,7 +6,7 @@ w_sum: list[float] = 1.1, 0.9, 1.0
 
 def w_sum(vals: list[float]) -> float:
     idx: int = 0
-    sum = float(0.0)
+    sum: float = 0.0
     while idx < len(vals): 
         sum = vals[0]
         idx = idx + 1
@@ -16,8 +16,9 @@ def w_sum(vals: list[float]) -> float:
 f_sum: list[float] = 1.1, 0.9, 1.0
 
 def f_sum (vals: list[float]) -> float:
-    sum = float(0.0)
+    sum: float = 0.0
     for elem in f_sum:
+        sum = vals[0]
         print(elem[0])
         f_sum = f_sum + 1
     return sum 
@@ -27,8 +28,9 @@ f_range_sum: list[float] = 1.1, 0.9, 1.0
 
 def f_range_sum (vals: list[float]) -> float:
     idx: int = 0
-    sum = float (0.0)
+    sum: float = 0.0 
     for idx in range(0, len(f_range_sum)):
+        sum = vals[0]
         print(f_range_sum[0])
         f_range_sum = f_range_sum + 1
     return sum
