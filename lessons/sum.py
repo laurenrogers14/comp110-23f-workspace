21977a63d1984ce6ae0a71ff3a47d95529d02a8d
Lauren Rogers <lfrog@unc.edu>
2023-10-18 13:58:53 -0400

    "Progress on lessons"

--- lessons/sum.py
+++ lessons/sum.py
@@ -1,5 +1,4 @@
 """Summing the elements of a list using different loops."""
-
 __author__ = "730711512"
 
 
@@ -12,24 +11,16 @@ def w_sum(vals: list[float]) -> float:
     return sum
 
 
-def f_sum (vals: list[float]) -> float:
+def f_sum(vals: list[float]) -> float:
     sum: float = 0.0
     for elem in vals:
         sum = sum + elem
     return sum 
 
 
-def f_range_sum (vals: list[float]) -> float:
+def f_range_sum(vals: list[float]) -> float:
     idx: int = 0
     sum: float = 0.0 
     for idx in range(0, len(vals)):
         sum = sum + vals[idx]
     return sum
-
-
-
-
-
-
-
-
