5c66a6f00c227f3aa2c8a84eb4fab68b05c732ce
Lauren Rogers <lfrog@unc.edu>
2023-10-27 18:39:32 -0400

    "Progress on Unit Tests"

--- lessons/zip_test.py
+++ lessons/zip_test.py
@@ -4,8 +4,9 @@ __author__ = "730711512"
 
 from lessons.zip import zip
 
+
 def test_non_empty_lists():
-    """Checks to see if the lists in the function returns a dictionary"""
+    """Checks to see if the lists in the function returns a dictionary."""
     list1 = []
     list2 = []
     result = zip(list1, list2)
@@ -13,18 +14,18 @@ def test_non_empty_lists():
 
 
 def test_empty_lists():
-    """Check to see that empty lists, return empty dictionaries"""
+    """Check to see that empty lists, return empty dictionaries."""
     list1 = ["Monday", "Tuesday"]
     list2 = [1, 2]
     result = zip(list1, list2)
-    expected_result ={"Monday": 1, "Tuesday": 2}
+    expected_result = {"Monday": 1, "Tuesday": 2}
     assert result == expected_result
 
 
 def test_lists_of_dif_lengths():
-    """Check to see if lists of different lengths still return empty dictionaries"""
+    """Check to see if lists of different lengths still return empty dictionaries."""
     list1 = ["Apple", "Banana", "Orange"]
-    list2 = [1,2,3]
+    list2 = [1, 2, 3]
     result_dict: dict[str, int] = zip(list1, list2)
     assert result_dict == {}
     
