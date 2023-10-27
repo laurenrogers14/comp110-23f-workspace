f6a22ae38343f7edbf31c7d1c43e11032baf699a
Lauren Rogers <lfrog@unc.edu>
2023-10-27 10:15:03 -0400

    "Progress on Dictionaries"

--- lessons/zip.py
+++ lessons/zip.py
@@ -3,6 +3,7 @@
 __author__ = "730711512"
 
 def zip(list1: list[str], list2: list[int]) -> dict[str, int]:
+    result_dict: dict[str, int] = {}
     if len(list1) != len(list2):
         return {}
     if list1 == {}:
@@ -10,6 +11,9 @@ def zip(list1: list[str], list2: list[int]) -> dict[str, int]:
     if list2 == {}:
         return{}
 
-result_dict: dict[str, int] = {"mahomes": 15, "kelce": 87}
-print(result_dict)
+    for i in range(len(list1)):
+        str_value = list1[i]
+        int_value = list2[i]
+        result_dict[str_value] = int_value
     
+    return result_dict
