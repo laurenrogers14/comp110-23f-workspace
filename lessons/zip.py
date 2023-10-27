36397c89391a5a4da44ce044a965a5ab92b2c804
Lauren Rogers <lfrog@unc.edu>
2023-10-27 10:27:44 -0400

    "Progress on Dictionaries"

--- lessons/zip.py
+++ lessons/zip.py
@@ -1,11 +1,13 @@
-"""Combing two lists into a dictionry"""
+"""Combing two lists into a dictionry."""
 
 __author__ = "730711512"
 
 def zip(list1: list[str], list2: list[int]) -> dict[str, int]:
+    """Return a dictionary given two lists."""
     result_dict: dict[str, int] = {}
     if not list1 or not list2 or len(list1) != len(list2):
-        return{}
+        return {} 
+    
 
     for i in range(len(list1)):
         str_value = list1[i]
