522d878c069a5e851312481f8b2fbf680318527c
Lauren Rogers <lfrog@unc.edu>
2023-10-27 10:29:43 -0400

    "Progress on Dictionaries"

--- lessons/zip.py
+++ lessons/zip.py
@@ -2,13 +2,12 @@
 
 __author__ = "730711512"
 
+
 def zip(list1: list[str], list2: list[int]) -> dict[str, int]:
     """Return a dictionary given two lists."""
     result_dict: dict[str, int] = {}
     if not list1 or not list2 or len(list1) != len(list2):
         return {} 
-    
-
     for i in range(len(list1)):
         str_value = list1[i]
         int_value = list2[i]
