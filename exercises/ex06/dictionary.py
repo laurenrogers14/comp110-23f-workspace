36d749d31e174f7308637c522d8b5ace599a4b81
Lauren Rogers <lfrog@unc.edu>
2023-11-01 18:17:21 -0400

    "Progress working with dictionaries."

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -7,29 +7,32 @@ def invert(dict1: dict[str, str]) -> dict[str, str]:
     """This function returns an inverted version of the orginal dictionary."""
     inverted_dict: dict[str, str] = {}
 
-    for key, value in dict1: 
-        if key in inverted_dict:
-            raise KeyError(f"Duplicate key {key} encountered while inverting!")
-        inverted_dict[value] == key
+    for key in dict1: 
+        value: str = dict1[key]
+        if value in inverted_dict:
+            raise KeyError(f"Duplicate key {value} encountered while inverting!")
+        else:
+            inverted_dict[value] = key
     
     return inverted_dict
 
 
 def favorite_color(dict2: dict[str, str]) -> str: 
     """This function returns the most popular color in the dictionary."""
-    color_count = {}
+    color_count: dict[str, int] = {}
     most_pop_color: str = ""
     max_count = 0
    
-    for key, value in dict2:
+    for key in dict2:
+        value: str = dict2[key]
         if value in color_count:
             color_count[value] += 1
         else:
-            color_count = 1
-        
-        if color_count[value] > max_count or (color_count[value] == max_count and dict2[value] == value):
-            max_count = color_count[value]
-            most_pop_color = value
+            color_count[value] = 1 
+    for elem in color_count:
+        if color_count[elem] > max_count:
+            max_count = color_count[elem]
+            most_pop_color = elem
 
     return most_pop_color
 
@@ -49,7 +52,7 @@ def count(input_list: list[str]) -> dict[str, int]:
 
 def alphabetizer(input_list2: list[str]) -> dict[str, list[str]]:
     """This function returns a dictionary of the letters and the lists of words that belong to that letter."""
-    result_dict2 = dict[str, list[str]] = {}
+    result_dict2: dict[str, list[str]] = {}
 
     for elem in input_list2:
         elem = elem.lower()
@@ -57,7 +60,7 @@ def alphabetizer(input_list2: list[str]) -> dict[str, list[str]]:
     
         if first_letter in result_dict2:
             result_dict2[first_letter].append(elem)
-        else:
+        if first_letter not in result_dict2:
             result_dict2[first_letter] = [elem]
         
     return result_dict2
