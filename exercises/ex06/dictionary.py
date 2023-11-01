fd44ce4496934c50df8be56b03afd4415300dbe3
Lauren Rogers <lfrog@unc.edu>
2023-11-01 17:02:47 -0400

    "Progress on working with dictionaries."

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -4,20 +4,19 @@ __author__ = "730711512"
 
 
 def invert(dict1: dict[str, str]) -> dict[str, str]:
+    """This function returns an inverted version of the orginal dictionary."""
     inverted_dict: dict[str, str] = {}
-    i: int = 0
 
     for key, value in dict1: 
-        if value in inverted_dict:
-            raise KeyError("Key values are unique and can only appear once!")
-        inverted_dict[value] = key
+        if key in inverted_dict:
+            raise KeyError(f"Duplicate key {key} encountered while inverting!")
+        inverted_dict[value] == key
     
     return inverted_dict
 
 
-
-
-def favorite_colors(dict2: dict[str, str]) -> str: 
+def favorite_color(dict2: dict[str, str]) -> str: 
+    """This function returns the most popular color in the dictionary."""
     color_count = {}
     most_pop_color = None
     max_count = 0
@@ -32,21 +31,24 @@ def favorite_colors(dict2: dict[str, str]) -> str:
             max_count = color_count[color]
             most_pop_color = color
 
+    return most_pop_color
+
 
 def count(input_list: list[str]) -> dict[str, int]:
+    """This function returns a dictionary of the counts of each of the items in the input list."""
     result_dict = {}
-    return_dict: dict[str, int]
-    i: int = 0
 
     for elem in input_list:
-        if input_list in return_dict:
+        if elem in result_dict:
             result_dict[elem] += 1
         else:
             result_dict[elem] = 1
 
     return result_dict
 
+
 def alphabetizer(input_list2: list[str]) -> dict[str, list[str]]:
+    """This function returns a dictionary of the letters and the lists of words that belong to that letter."""
     result_dict2 = {}
 
     for elem in input_list2:
@@ -62,7 +64,7 @@ def alphabetizer(input_list2: list[str]) -> dict[str, list[str]]:
 
 
 def update_attendance(dict3: dict[str, list[str]], day_of_week: str, student: str):
-
+    """This function should update the new attendance information."""
     if day_of_week in dict3:
         dict3[day_of_week].append(student)
     else:
